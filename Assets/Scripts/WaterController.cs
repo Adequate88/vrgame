cd56b8a4c7c6817912247ed0fd760c56b15fc491
Ale <alessandro.salo@epfl.ch>
2025-03-24 11:53:02 +0100

    Testing

---
+++ Assets/Scripts/WaterController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class WaterController : MonoBehaviour
+{
+    // Maximum y position the object can reach
+    public float waterVelocity;
+
+    // Reference to a GameObject
+    public GameObject roof;
+    public GameObject floor;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Set the initial position of the object
+        transform.position = floor.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increase the y position of the object if it is below the maximum water level
+        if (transform.position.y < roof.transform.position.y) {
+            transform.position += new Vector3(0, waterVelocity * Time.deltaTime, 0);
+        }
+    }
+}
