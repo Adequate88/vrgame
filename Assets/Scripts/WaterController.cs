1a19f85aaf589a6a53a5a5edd0ae48c5d861d8e3
Ale <alessandro.salo@epfl.ch>
2025-05-08 09:35:12 +0200

    Puzzle Lights, Room complete except for hiding and gravity

--- Assets/Scripts/WaterController.cs
+++ Assets/Scripts/WaterController.cs
@@ -8,14 +8,18 @@ public class WaterController : MonoBehaviour
     // Reference to a GameObject
     public GameObject roof;
     public GameObject floor;
-    
+    public GameObject underWaterProfile;
     public PuzzleController puzzleController;
 
+    private Camera cam;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // Set the initial position of the object
         transform.position = floor.transform.position;
+        cam = Camera.main;
+        
     }
 
     // Update is called once per frame
@@ -24,6 +28,11 @@ public class WaterController : MonoBehaviour
         // Increase the y position of the object if it is below the maximum water level
         if (transform.position.y < roof.transform.position.y && puzzleController.puzzleComplete) {
             transform.position += new Vector3(0, waterVelocity * Time.deltaTime, 0);
+        }
+
+        if (cam.transform.position.y < transform.position.y)
+        {
+            underWaterProfile.SetActive(true);
         }
     }
 }
