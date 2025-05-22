403a8a09ca52f763dcee7791a58fb8e2037d1048
Ale <alessandro.salo@epfl.ch>
2025-05-22 10:56:36 +0200

    Made tutorial settings global

--- Assets/Miscellaneous_Menu_Scene/Settings_Menu/SettingsMenu.cs
+++ Assets/Miscellaneous_Menu_Scene/Settings_Menu/SettingsMenu.cs
@@ -1,13 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Comfort;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Turning;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 public class SettingsMenu : MonoBehaviour
 {
     public GameObject tunnelingVignette;
+    public ControllerInputActionManager controllerInputActionManager;
+    public void Start()
+    {
+        // Initialize the settings menu based on the current settings
+        tunnelingVignette.SetActive(SettingsData.tunnelingVignetteEnabled);
+        controllerInputActionManager.smoothTurnEnabled = SettingsData.smoothTurnEnabled;
+
+    }
 
     public void ToggleRotationMode()
     {
         SettingsData.smoothTurnEnabled = !SettingsData.smoothTurnEnabled;
+        controllerInputActionManager.smoothTurnEnabled = SettingsData.smoothTurnEnabled;
         Debug.Log($"Smooth Turn Enabled is now: {SettingsData.smoothTurnEnabled}");
     }
 
