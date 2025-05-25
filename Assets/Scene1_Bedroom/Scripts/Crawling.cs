4a86bfc39d8bcdcf4ef37dc5ac155914e4fba36f
anoukpaulino <anouk.paulino@epfl.ch>
2025-05-25 20:04:38 +0200

    fix bedroom puzzle

--- Assets/Scene1_Bedroom/Scripts/Crawling.cs
+++ Assets/Scene1_Bedroom/Scripts/Crawling.cs
@@ -37,7 +37,7 @@ public class Crawling : ContinuousMoveProvider
     private float requiredSwipeDist = 0.3f;
     private float currentSwipeDist = 0f;
 
-    public bool isCrawlingUnderBed = false;
+    private bool isCrawlingUnderBed = false;
     private Vector3 initialScale;
 
     // Tag objects
